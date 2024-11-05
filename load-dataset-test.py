643f746b26bc8e42cb1110943316daa0e9e3dd55
Peter Kompiel <peter.kompiel@cedis.fu-berlin.de>
2024-11-05 10:43:43 +0100

    seed factor

--- load-dataset-test.py
+++ load-dataset-test.py
@@ -1,10 +1,16 @@
 from datasets import load_dataset, DatasetDict
 
-dataset = DatasetDict()
-
 # Laden des Datasets von der bereinigten CSV-Datei
-dataset["train"] = load_dataset("audiofolder", data_dir="/Users/peterkompiel/python_scripts/asr4memory/processing_files/whisper-train/_output/eg085_01_01/", split="train")
-dataset["test"] = load_dataset("audiofolder", data_dir="/Users/peterkompiel/python_scripts/asr4memory/processing_files/whisper-train/_output/eg085_01_01/", split="test")
+dataset = load_dataset("audiofolder", data_dir="/Users/peterkompiel/python_scripts/asr4memory/processing_files/whisper-train/_output/eg085_01_01")
+
+# Dataset in Trainings- und Testsets aufteilen mit festem Seed
+split_dataset = dataset['train'].train_test_split(test_size=0.2, seed=42)  # 42 ist ein Beispielwert für den Seed
+
+# Erstellen eines DatasetDict-Objekts
+dataset = DatasetDict({
+    'train': split_dataset['train'],
+    'test': split_dataset['test']
+})
 
 # Überprüfen des ersten Datensatzes
 print(dataset['train'][0])
